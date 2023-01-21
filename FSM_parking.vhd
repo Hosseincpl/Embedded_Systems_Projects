0027b636f6bc45affeac81949535c6d26f8812d5
Hossein Karami <72271858+Hosseincpl@users.noreply.github.com>
2023-01-21 18:15:24 +0330

    Update FSM_parking.vhd
    add some comments

--- FSM_parking.vhd
+++ FSM_parking.vhd
@@ -1,3 +1,8 @@
+-- s1 => empty space
+-- s2 => entering
+-- s3 => filled space
+-- s4 => leaving
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 -------------------------------------------
@@ -10,14 +15,14 @@ entity fsm_eg is
 end fsm_eg;
 -------------------------------------------
 architecture mult_seg_arch of fsm_eg is
-    type eg_state_type is (A, B, C, D);
+    type eg_state_type is (s1, s2, s3, s4);
     signal state_reg, state_next: eg_state_type;
 begin
 -- state register
     process(clk, reset)
     begin
         if (reset = '1') then 
-            state_reg <= A;
+            state_reg <= s1;
         elsif (clk' event and clk = '1') then
             state_reg <= state_next;
         end if;
@@ -27,33 +32,33 @@ begin
     process (state_reg, a, b)
     begin 
         case state_reg is
-        when A =>
+        when s1 =>
             if a='1' then
                 if b='1' then
-                    state_next <= C;
+                    state_next <= s3;
                 else
-                    state_next <= B;
+                    state_next <= s2;
                 end if;
             else
                 state_next <= state_reg;
             end if;
-        when B =>
+        when s2 =>
             if b = '1' then
-                state_next <= C;
+                state_next <= s3;
             elsif a = '0' then
-                state_next <= A;
+                state_next <= s1;
             end if;
-        when C =>
+        when s3 =>
             if a = '0' then
-                state_next <= D;
+                state_next <= s4;
             elsif b = '0' then
-                state_next <= B;
+                state_next <= s2;
             end if; 
-        when D =>
+        when s4 =>
             if a = '1' then 
-                state_next <= C;
+                state_next <= s3;
             elsif b = '0' then
-                state_next <= A;
+                state_next <= s1;
             end if;
         end case;
     end process;
@@ -62,14 +67,14 @@ begin
     process (state_reg)
     begin 
         case state_reg is
-            when A =>
-                Q <= A;
-            when B =>
-                Q <= B;
-            when C =>
-                Q <= C;
-            when D =>
-                Q <= D;
+            when s1 =>
+                Q <= "00";
+            when s2 =>
+                Q <= "10";
+            when s3 =>
+                Q <= "11";
+            when s4 =>
+                Q <= "01";
         end case;
     end process;
 end mult_seg_arch;
